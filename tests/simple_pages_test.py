05e8d47208b7879873c0f81e76c2081c6ca2583f
Jkowal012 <jk262@njit.edu>
2022-03-07 03:45:08 -0500

    adding pytest

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,7 +4,7 @@ def test_request_main_menu_links(client):
     """This makes the navbar"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link active" aria-current="page" href="{{ url_for('simple_pages.show', page="index")}}">Home</a>' in response.data
+    assert b'<a class="nav-link active" aria-current="page" href="/index">Home</a>' in response.data
     assert b'<a class="nav-link" href="/page1">Github</a>' in response.data
     assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
