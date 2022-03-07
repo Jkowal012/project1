f2de1d15a92ff8d34425a85b09ad1e70b6a4d550
Jkowal012 <jk262@njit.edu>
2022-03-07 04:17:18 -0500

    adding pytest

--- app/context_processors/__init__.py
+++ app/context_processors/__init__.py
@@ -2,7 +2,7 @@ from os import getenv
 import datetime
 
 def utility_text_processors():
-    message = "hello world"
+    message = 'For educational purposes only; IS218'
 
     def deployment_environment():
         return getenv('FLASK_ENV', None)
