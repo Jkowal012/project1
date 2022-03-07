5318a5e6ab39dc2ea48baa61c6f0b908e39b5fc6
Jkowal012 <jk262@njit.edu>
2022-03-07 03:10:45 -0500

    adding pytest

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,7 +4,7 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/index">Home</a>' in response.data
+    assert b'<a class="nav-link" href="/index">Index</a>' in response.data
     assert b'<a class="nav-link" href="/page1">Github</a>' in response.data
     assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
@@ -26,19 +26,19 @@ def test_request_page2(client):
     """This makes the index page"""
     response = client.get("/page2")
     assert response.status_code == 200
-    assert b"Page 2" in response.data
+    assert b"Docker and Dockerhub" in response.data
 
 def test_request_page3(client):
     """This makes the index page"""
     response = client.get("/page3")
     assert response.status_code == 200
-    assert b"Page 3" in response.data
+    assert b"Python/Flask" in response.data
 
 def test_request_page4(client):
     """This makes the index page"""
     response = client.get("/page4")
     assert response.status_code == 200
-    assert b"Page 4" in response.data
+    assert b"Continuous Development/Continuous Deployment" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
