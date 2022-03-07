4118ab4bab5ca3fbf706e1da5e8e5053022dbdc0
Jkowal012 <jk262@njit.edu>
2022-03-07 03:15:25 -0500

    adding pytest

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,7 +4,7 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/index">Index</a>' in response.data
+    assert b'<a class="nav-link" href="/index">Home</a>' in response.data
     assert b'<a class="nav-link" href="/page1">Github</a>' in response.data
     assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
